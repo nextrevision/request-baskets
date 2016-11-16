c0ffaaa2530d8e70335d0f566deb48b40417e993
Vladimir L <vladimir_l@gmx.net>
2016-11-16 22:51:29 +0100

    added Fork me on GitHub ribbon, just for demo site only

--- web_index.html.go
+++ web_index.html.go
@@ -23,6 +23,11 @@ const (
     #baskets li:before { content: "\f291"; font-family: "FontAwesome"; position: absolute; left: 0px; top:0px; }
   </style>
 
+  <link rel="stylesheet" href="https://darklynx.github.io/strapdown/v/gh-fork-ribbon/gh-fork-ribbon.css">
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="https://darklynx.github.io/strapdown/v/gh-fork-ribbon/gh-fork-ribbon.ie.css">
+  <![endif]-->
+
   <script>
   (function($) {
     var basketsCount = 0;
@@ -135,6 +140,8 @@ const (
   </script>
 </head>
 <body>
+  <a class="github-fork-ribbon left-top" href="https://github.com/darklynx/request-baskets" title="Fork me on GitHub">Fork me on GitHub</a>
+
   <!-- Fixed navbar -->
   <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
